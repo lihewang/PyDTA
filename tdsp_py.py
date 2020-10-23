219111be68cb57f64f9c08d65cc5e0cba4064f18
lihewang <lihe_wang@hotmail.com>
2020-10-23 14:44:56 -0400

    Add files via upload

--- tdsp_py.py
+++ tdsp_py.py
@@ -3,6 +3,8 @@
 Created on Fri Sep 25 09:14:22 2020
 
 @author: lihe.wang
+python -m cProfile -o pf.txt tdsp_py.py
+cprofilev -f pf.txt
 """
 
 import numpy as np
@@ -13,7 +15,7 @@ import timeit
 #Time Dependent Shortest Path
 class tdsp:
 
-    def __init__(self, num_nodes, link_attribute, args):
+    def __init__(self, num_nodes, next_links, link_attribute, args):
         #Node impedance and index
         self.num_nodes = num_nodes
         self.node_imp = np.array(np.full(self.num_nodes,args[1]), dtype='f') 
@@ -21,16 +23,18 @@ class tdsp:
         self.node_dist = np.zeros(self.num_nodes, dtype=np.float32)
         self.node_toll = np.zeros(self.num_nodes, dtype=np.float32)
         self.my_heap = hp.heap(self.num_nodes, self.node_imp)
+        self.next_links = next_links
         self.link_attribute = link_attribute
         self.args = args
         
     def build(self, sp_task):
+        
         self.o_node_index = sp_task[0]
         self.d_node_index = sp_task[1]        
         self.start_ts = sp_task[2]
         self.node_imp[self.o_node_index] = 0
         self.my_heap.insert(self.o_node_index)
-        self.parent_node = pd.Series(np.full(self.num_nodes,-1), index=range(self.num_nodes))
+        self.parent_node = np.array(np.full(self.num_nodes,-1), dtype='i')
         self.popped = np.zeros(self.num_nodes, dtype='i')
         
         while not self.my_heap.is_empty():
@@ -40,9 +44,6 @@ class tdsp:
             #Find destination
             if self.top_node_index == self.d_node_index:
                 break
-            #No next link
-            if not self.top_node_index in self.link_attribute[:,0]:
-                continue
             
             #Calculate time step
             self.curr_ts = int(self.node_time[self.top_node_index]//15) + self.start_ts
@@ -50,14 +51,14 @@ class tdsp:
                 self.curr_ts -= 96
             self.curr_ts_col = self.curr_ts + 3
             
-            #Next links index           
-            self.next_links = np.where(self.link_attribute[:,0] == self.top_node_index)           
-
-            for self.i in self.next_links[0]:
+            #Next links index                  
+            self.nxlinks = self.next_links[self.top_node_index]
+            for self.i in self.nxlinks[0]:
+                #self.i = self.link
                 self.b_node = int(self.link_attribute[self.i, 1])
                 self.t = self.link_attribute[self.i, self.curr_ts_col]
                 self.imp = self.t + self.args[2] * self.link_attribute[self.i, 3]
-                #Path can't be backward       
+                #Path can't be backward 
                 if self.b_node == self.top_node_index or self.popped[self.b_node] == 1:
                     continue
                 #print('B node ' + str(self.b_node))
@@ -79,7 +80,7 @@ class tdsp:
                 #zones can't be transpassed    
                 if self.top_node_index <= self.args[0] and self.top_node_index != self.o_node_index:
                     continue
-        
+
         return self.parent_node 
 
     def trace(self, sp_task, parent_node):
@@ -112,7 +113,7 @@ if __name__ == "__main__":
     node_index = pd.Series(node.index, index=node['N'], name='node_index')
     index_node = pd.Series(node['N'], index=node.index, name='node_number')
     d_node = node[node['DTA_Type'].isin([90,91])][['DTA_Type', 'DNGRP']]    #Decision nodes
-
+    
     link_attribute = link[['DISTANCE','TOLL']]
     link_attribute.insert(0, column='B', value=node_index[link['B']].values)
     link_attribute.insert(0, column='A', value=node_index[link['A']].values)
@@ -120,14 +121,20 @@ if __name__ == "__main__":
     index_a = pd.Series(node_index[link['A']].values, index=link.index)
     index_b = pd.Series(node_index[link['B']].values, index=link.index)
     link_AB = pd.DataFrame({'link_index':link.index},index=index_a.astype(str)+'_'+index_b.astype(str))
-      
+    df_index_a = pd.DataFrame({'anode':index_a, 'link':index_a.index})
+    a_link = df_index_a.groupby('anode')['link'].apply(list)
+    
+    next_links = pd.DataFrame(range(len(node_index))).join(a_link).drop(columns=0)
+    for row in next_links.loc[next_links.link.isnull(), 'link'].index:
+        next_links.at[row, 'link'] = []
+
     #Create time for 96 time steps
     time = link['DISTANCE']/link['FFSPEED']*60
     for i in range(0,96):
         link_attribute.loc[:,'time'+str(i+1)] = time
     
     t1 = timeit.default_timer()
-    my_sp = tdsp(len(node_index), link_attribute.to_numpy(dtype='f'), args.to_numpy())
+    my_sp = tdsp(len(node_index), next_links.to_numpy(), link_attribute.to_numpy(dtype='f'), args.to_numpy())
     sp_task['O'] = node_index[sp_task['O']]
     sp_task['D'] = node_index[sp_task['D']]
     #build path
